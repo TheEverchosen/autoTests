8122a4b940fec5159d46cd53c5b0585cd9dbd7d0
TheEverchosen <starobelsk97@gmail.com>
2019-07-25 16:44:24 +0300

    Added search sorting test

--- tests/search-test.ts
+++ tests/search-test.ts
@@ -17,8 +17,8 @@ fixture `Search`
             .click('[name="login"]')
     });
 
-test('Search test', async t => {
-    
+ test('Search test', async t => {
+
     await t
     .typeText('.form-control ', 'Duck')
     .pressKey('enter')
@@ -28,4 +28,23 @@ test('Search test', async t => {
     await t
     .expect(temp).contains("Duck")
 });
+
+test('Search sorting test', async t => {
+    
+    await t
+    .typeText('.form-control ', 'Duck')
+    .pressKey('enter')
+    .click('[href="http://ip-5236.sunline.net.ua:38015/search?query=Duck&page=1&sort=price"]');
+
+    let temp = await Selector(".products").innerText;
+    let priceArray = temp.replace(/[^z0-9]/g, ' ').split(' ').filter(Number).sort();
+    console.log("AHAHAAH: ", priceArray);
+    
+
+    await t
+    .expect(temp).contains("Duck")
+    .expect(priceArray.slice(1).every((item, i) => priceArray[i] <= item)).ok();
+
+});
+
 
