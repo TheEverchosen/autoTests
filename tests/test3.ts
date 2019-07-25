1827b01a88e63e7fd996f75034def6cab3d592df
TheEverchosen <starobelsk97@gmail.com>
2019-07-25 11:59:59 +0300

    Code styling and comments

--- tests/test3.ts
+++ tests/test3.ts
@@ -20,19 +20,27 @@ test('Second homework test', async t => {
     
 
     await t
+        //company + phone
         .typeText('input[name="company"]', faker.company.companySuffix())
         .typeText('input[name="tax_id"]', "NL999999999B99")
+        .typeText('input[name="phone"]', faker.phone.phoneNumberFormat(0))
+        //address
         .typeText('input[name="address1"]', faker.address.streetAddress())
         .typeText('input[name="address2"]', faker.address.secondaryAddress())
         .typeText('input[name="postcode"]', faker.address.zipCode("#####"))
-        .typeText('input[name="phone"]', faker.phone.phoneNumberFormat(0))
         .typeText('input[name="city"]', faker.address.city())
+
+        //name 
         .typeText('input[name="firstname"]', faker.name.firstName())
         .typeText('input[name="lastname"]', faker.name.lastName())
+
+        //country + state 
         .click('.form-control[name="country_code"]')
         .click(Selector('option').withAttribute('value', 'US'))
         .click('.form-control[name="zone_code"]')
         .click(Selector('option').withAttribute('value', randomState))
+
+        //email + password + finalising
         .typeText('[name="customer_form"] [name="email"]', email)
         .typeText('[name="customer_form"] [name="password"]', password)
         .typeText('[name="customer_form"] [name="confirmed_password"]', password)
@@ -41,6 +49,7 @@ test('Second homework test', async t => {
 
     const expectedText = await Selector('.alert.alert-success').innerText;
     const expectedPageUrl = ClientFunction(() => window.location.href);
+    
     await t
         .expect(expectedText.replace(/[^A-Za-z0-9]/g, ' ').trim()).eql(successMsg)
         .expect(expectedPageUrl()).contains('');
