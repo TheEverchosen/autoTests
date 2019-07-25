de3f4234356930a8000b278c2adf3dc23c6533af
TheEverchosen <starobelsk97@gmail.com>
2019-07-25 11:34:52 +0300

    Second test works properly

--- tests/registration-test.ts
+++ tests/registration-test.ts
@@ -1,23 +1,28 @@
 import {ClientFunction, Selector} from 'testcafe';
 import * as faker  from "faker";
 
-let successMsg = 'Your customer account has been created';
+
 
 fixture `Registration page`
     .page `http://ip-5236.sunline.net.ua:38015/create_account`;
 
-test('My first registration test', async t => {
+test('Ammended homework test', async t => {
+    
     const email = faker.internet.email();
+    const password = faker.internet.password(10);
+    const successMsg = 'Your customer account has been created';
+
     await t
-        .typeText('input[name="firstname"]', 'First Name')
-        .typeText('input[name="lastname"]', 'Last Name')
+        .typeText('input[name="firstname"]', faker.name.firstName())
+        .typeText('input[name="lastname"]', faker.name.lastName())
         .click('.form-control[name="country_code"]')
         .click(Selector('option').withAttribute('value', 'UA'))
         .typeText('[name="customer_form"] [name="email"]', email)
-        .typeText('[name="customer_form"] [name="password"]', email)
-        .typeText('[name="customer_form"] [name="confirmed_password"]', email)
+        .typeText('[name="customer_form"] [name="password"]', password)
+        .typeText('[name="customer_form"] [name="confirmed_password"]', password)
         .click('[name="newsletter"]')
         .click('[name="create_account"]');
+
     const expectedText = await Selector('.alert.alert-success').innerText;
     const expectedPageUrl = ClientFunction(() => window.location.href);
     await t
